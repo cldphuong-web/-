9a7afdd6a5d805a55c6d5ca40de09b5b40f263e0
cldphuong-web <cldphuong@gmail.com>
2026-03-05 22:24:36 +0800

    version 8: PHP

---
+++ about.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>黃褱方簡介</title>
+	<style type="text/css">
+		* { font-family:"標楷體"; margin-left:auto; margin-right:auto;}
+		h1 {color:blue; font-size:60px;}
+		h2 {color:#33ff33; font-size:40px;}
+	</style>
+	<script>
+		function change1() {
+  			document.getElementById("pic").src = "mountain.jpg";
+  			document.getElementById("h2text").innerText = "靜宜資管";
+		}
+
+		function change2() {
+  			document.getElementById("pic").src = "cliff.jpg";
+  			document.getElementById("h2text").innerText = "來自越南";
+		}
+	</script>
+
+
+	
+
+
+</head>
+<body>
+	<table width="70%">
+		<tr>
+			<td>
+				<img src="cliff.jpg" width="110%"id="pic" onmouseover="change1()" onmouseout="change2()"></img>
+			</td>
+
+			<td>
+<img src="cliff.jpg"></img>
+
+<h1>黃褱方</h1>
+<h2 id="h2text">Shawn Double H</h2>
+</td>
+		</tr>
+	</table>
+
+<table width="70%" border="1">
+	<tr>
+<td>
+
+自我網頁<br>
+
+Youtube channel:<br>
+<a href="www.youtube.com/@afangtw0110">www.youtube.com/@afangtw0110</a><br>
+<a href="www.youtube.com/@taiwanitreal">www.youtube.com/@taiwanitreal</a><br>
+Facebook: <br>
+<a href ="facebook.com/shawn2609?mibextid=ZbWKwL"
+target="_blank">facebook.com/shawn2609?mibextid=ZbWKwL</a><br>
+Contact me: <br>
+ <a href="mailto:cldphuong@gmail.com">cldphuong@gmail.com</a><br>
+ </td>
+
+			<td>
+ 大象席地而坐電影配樂<br>
+	<audio controls>
+		<source src="elephant.mp3" type="audio/mP3">
+	</audio><br>
+</td>
+
+			<td>
+	不要去臺灣<br>
+	<iframe src="https://www.youtube.com/embed/pW88QFpHXa8" allowfullscreen></iframe>
+	</td>
+		</tr>
+	</table>
+
+
+<?php echo date("Y-m-d") ?></body>
+</html>
